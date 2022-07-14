4036fce2ac97774dc928af5ba3ddbf3cec31e144
Reed Harston <rtharston@gmail.com>
2022-07-13 20:48:46 -0700

    Prompts user for input to print, using std::io

--- src/lox/run.rs
+++ src/lox/run.rs
@@ -10,7 +10,28 @@ pub fn run_file(path: &str) {
 
 // Show a prompt to the user and run their input line by line
 pub fn run_prompt() {
-    run("run prompt")
+    use std::io::{stdin,stdout,Write};
+    let stdin = stdin();
+    let mut stdout = stdout();
+    let mut input = String::new();
+
+    loop {
+        print!("> ");
+        let _ = stdout.flush();
+        
+        match stdin.read_line(&mut input) {
+            Ok(n) => {
+                if n == 0 {
+                    // If they pressed ctrl+d we get no bytes and exit the loop
+                    break;
+                }
+
+                run(&input.trim());
+                input.clear()
+            },
+            Err(e) => println!("Attempting to read line caused the following error:\n\t{}", e),
+        }
+    }
 }
 
 // Run the code
